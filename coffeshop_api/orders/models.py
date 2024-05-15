7bf1f49c9da0d6ebcf13cd204a8e491f3d89d8ef
Roman Ivanov <sitdoff@gmail.com>
2024-05-15 13:01:32 +0300

    add orderitem model

--- coffeshop_api/orders/models.py
+++ coffeshop_api/orders/models.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.db import models
+from django.db.models.fields.related import RelatedField
+from goods.models import ProductModel
 
 # Create your models here.
 
@@ -12,8 +14,26 @@ class OrderModel(models.Model):
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
 
+    items: RelatedField
+
     class Meta:
         ordering = ("-created",)
 
     def __str__(self) -> str:
-        return f"Order {self.id}"
+        return f"Order {self.pk}"
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(OrderModel, on_delete=models.CASCADE, related_name="items", verbose_name="Товары")
+    product = models.ForeignKey(ProductModel, on_delete=models.CASCADE, related_name="order_item", verbose_name="Товар")
+    price = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Цена")
+    quantity = models.PositiveIntegerField(default=1, verbose_name="Количество")
+
+    def __str__(self) -> str:
+        return f"Item {self.pk}"
+
+    def get_cost(self):
+        return self.price * self.quantity
