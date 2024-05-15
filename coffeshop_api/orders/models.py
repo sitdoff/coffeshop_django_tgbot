22a4a1b0aa914518774ca3782f1cf9058a681551
Roman Ivanov <sitdoff@gmail.com>
2024-05-15 12:38:32 +0300

    add order model

--- coffeshop_api/orders/models.py
+++ coffeshop_api/orders/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+
+class OrderModel(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="orders", verbose_name="Пользователь"
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self) -> str:
+        return f"Order {self.id}"
