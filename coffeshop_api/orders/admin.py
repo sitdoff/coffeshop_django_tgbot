575b4385f20ced85e97720b6f8140608444e1217
Roman Ivanov <sitdoff@gmail.com>
2024-05-16 12:13:11 +0300

    add admin models

--- coffeshop_api/orders/admin.py
+++ coffeshop_api/orders/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import OrderItemModel, OrderModel
+
+
 # Register your models here.
+class OrderItemModelInLine(admin.StackedInline):
+    model = OrderItemModel
+    max_num = 2
+
+
+@admin.register(OrderModel)
+class OrderModelAdmin(admin.ModelAdmin):
+    inlines = [OrderItemModelInLine]
