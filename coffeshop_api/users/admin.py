1cd43b477cf48e33c6f67bb73327276712cc9aeb
Roman Ivanov <sitdoff@gmail.com>
2024-05-23 15:02:42 +0300

    add TelegraUserAdmin /users/admin.py

--- coffeshop_api/users/admin.py
+++ coffeshop_api/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import TelegramUser
+
 
 # Register your models here.
+class TelegramUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (("Telegram ID", {"fields": ("telegram_id",)}),)
+
+
+admin.site.register(TelegramUser, TelegramUserAdmin)
