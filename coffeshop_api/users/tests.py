bf931992cc3a30f532f48f3cee384d7630e500cc
Roman Ivanov <sitdoff@gmail.com>
2024-05-24 13:51:39 +0300

    add tests TelegramUserSerializer

--- coffeshop_api/users/tests.py
+++ coffeshop_api/users/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
+from .models import TelegramUser
+from .serializers import TelegramUserSerializer
+
+
 # Create your tests here.
+class TestTelegraUserSerializer(TestCase):
+    def setUp(self) -> None:
+        self.user = TelegramUser.objects.create(username="Test user", email="test_user@mail.com", telegram_id="123456")
+        self.user.set_password("password")
+        self.serializer = TelegramUserSerializer(instance=self.user)
+        return super().setUp()
+
+    def test_user_serialization(self):
+        self.assertEqual(self.serializer.data["username"], self.user.username)
+        self.assertEqual(self.serializer.data["email"], self.user.email)
+        self.assertEqual(self.serializer.data["telegram_id"], self.user.telegram_id)
+        self.assertEqual(self.serializer.data["is_active"], True)
+        self.assertEqual(self.serializer.data["is_staff"], False)
+        self.assertEqual(self.serializer.data["is_staff"], False)
