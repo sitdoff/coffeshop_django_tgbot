4bd61fb7aa0fa0339aee6f9f5e4187903f86261a
Roman Ivanov <sitdoff@gmail.com>
2024-05-30 14:23:13 +0300

    rework TelegramOrderFactory /orders/order_factory.py

--- coffeshop_api/orders/order_factory.py
+++ coffeshop_api/orders/order_factory.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 from cart.cart import Cart
 from django.conf import settings
 from django.http import HttpRequest
+from goods.models import ProductModel
 
 from .models import OrderItemModel, OrderModel
 
@@ -30,39 +31,40 @@ class TelegramOrderFactory(OrderFactory):
     Class for creating orders from telegram.
     """
 
-    def create_order(self, request: HttpRequest):
+    def create_order(self, request: HttpRequest, cart: Cart):
         """
         Creates and returns an order object.
         """
-        if self.check_cart(request):
+        if cart is None:
+            raise Exception("Нет корзины")
+        if self.check_cart(cart):
             order = OrderModel.objects.create(owner=request.user)
-            items = self.get_items(request)
+            items = self.get_items(cart)
             for item in items:
                 order.items.add(item)
             return order
 
-    def check_cart(self, request: HttpRequest):
+    def check_cart(self, cart: Cart):
         """
         Checks the presence of a cart in the session and the presence of goods in it.
         """
-        if settings.CART_SESSION_ID not in request.session:
+        if not "items" in cart.cart and not "ordered" in cart.cart:
             raise Exception("Нет корзины")
-        if len(request.session[settings.CART_SESSION_ID]) == 0:
+        if not cart.cart["ordered"]:
             raise Exception("Корзина пуста")
         return True
 
-    def get_items(self, request):
+    def get_items(self, cart: Cart):
         """
         Returns ordered items objects from the cart to be attached to the order.
 
         At the end, clears the order cart.
         """
-        cart = Cart(request)
         items = []
         for item_data in cart:
             item = OrderItemModel.objects.create(
                 order=None,
-                product=item_data["product"],
+                product=ProductModel.objects.get(pk=item_data["product_id"]),
                 price=item_data["price"],
                 quantity=item_data["quantity"],
             )
