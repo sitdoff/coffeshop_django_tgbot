05c7631a4cf3d07d0cb8abd6eb616c4392e7654a
Roman Ivanov <sitdoff@gmail.com>
2024-06-03 12:04:49 +0300

    update CartSeraliaer /cart/seralizers.py

--- coffeshop_api/cart/serializers.py
+++ coffeshop_api/cart/serializers.py
@@ -9,7 +9,7 @@ class CartSerializer(serializers.Serializer):
     """
 
     items = serializers.DictField(source="cart.items")
-    total_price = serializers.DecimalField(source="get_total_price", max_digits=5, decimal_places=2)
+    total_price = serializers.DecimalField(source="get_total_price", max_digits=5, decimal_places=2, read_only=True)
 
     def create(self, validated_data):
         return Cart(data=validated_data)
