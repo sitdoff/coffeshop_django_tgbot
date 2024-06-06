0a122185f07308bb6a0dc8323df163036f7c3dde
Roman Ivanov <sitdoff@gmail.com>
2024-06-06 13:21:51 +0300

    add pagination settings /core/settings.py

--- coffeshop_api/core/settings.py
+++ coffeshop_api/core/settings.py
@@ -156,6 +156,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 CART_SESSION_ID = "cart"
