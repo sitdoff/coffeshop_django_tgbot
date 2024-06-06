228a4721db75c4de229974534e7563dcb1c678aa
Roman Ivanov <sitdoff@gmail.com>
2024-06-06 13:04:24 +0300

    update OrderViewSet /orders/views.py

--- coffeshop_api/orders/views.py
+++ coffeshop_api/orders/views.py
@@ -1,30 +1,17 @@
 from typing import Any
 
 from cart.serializers import CartSerializer
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet
 
-from .models import OrderItemModel, OrderModel
+from .models import OrderModel
 from .order_factory import TelegramOrderFactory
 from .serializers import OrderSerializer
 
 # Create your views here.
-
-
-# class OrderViewSet(ReadOnlyModelViewSet):
-#     """
-#     OrederModel viewset.
-#     """
-#
-#     queryset = OrderModel.objects.all()
-#     serializer_class = OrderSerializer
-#
-#     def get_queryset(self):
-#         queryset = OrderModel.objects.filter(owner=self.request.user.pk)
-#         return queryset
 
 
 class OrderViewSet(ModelViewSet):
@@ -34,6 +21,7 @@ class OrderViewSet(ModelViewSet):
 
     queryset = OrderModel.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         queryset = OrderModel.objects.filter(owner=self.request.user.pk)
@@ -43,9 +31,10 @@ class OrderViewSet(ModelViewSet):
         cart_serializer = CartSerializer(data=request.data)
         if cart_serializer.is_valid():
             cart = cart_serializer.save()
-            order_factory = TelegramOrderFactory()
-            order = order_factory.create_order(request=request, cart=cart)
-            return Response(data=OrderSerializer(order).data)
-        # print(request.data)
-        # print(cart.cart)
-        # print(order)
+            try:
+                order_factory = TelegramOrderFactory()
+                order = order_factory.create_order(request=request, cart=cart)
+            except ValueError as e:
+                return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data=OrderSerializer(order).data, status=status.HTTP_201_CREATED)
+        return Response(cart_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
