88932a7bbb5ca0381b7c259ed6071eedef97c59c
Roman Ivanov <sitdoff@gmail.com>
2024-06-07 12:09:07 +0300

    update TelegramUserAdmin /users/admin.py

--- coffeshop_api/users/admin.py
+++ coffeshop_api/users/admin.py
@@ -6,6 +6,7 @@ from .models import TelegramUser
 
 # Register your models here.
 class TelegramUserAdmin(UserAdmin):
+    list_display = ["telegram_id", "username", "email", "is_active"]
     fieldsets = UserAdmin.fieldsets + (("Telegram ID", {"fields": ("telegram_id",)}),)
 
 
