3abe2c134662d37a9282c08e25425cd3aa465698
Roman Ivanov <sitdoff@gmail.com>
2024-06-14 10:55:56 +0300

    update settigs strucrure

---
+++ coffeshop_api/core/settings/development.py
@@ -0,0 +1,21 @@
+import os
+
+from core.settings.base import *
+
+from .base import *
+
+# load_dotenv()
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
+    }
+}
