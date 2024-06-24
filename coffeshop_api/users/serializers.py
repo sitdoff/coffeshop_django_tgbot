732cd5da7127a3c23bc6887427c85f6e98de5388
Roman Ivanov <sitdoff@gmail.com>
2024-06-24 12:25:16 +0300

    add comments /users/serializers.py

--- coffeshop_api/users/serializers.py
+++ coffeshop_api/users/serializers.py
@@ -7,15 +7,27 @@ from .models import TelegramUser
 
 
 class TelegramUserSerializer(serializers.ModelSerializer):
+    """
+    User serializer.
+    """
+
     class Meta:
         model = TelegramUser
         fields = "__all__"
 
 
 class TelegramAuthSerializer(serializers.Serializer):
+    """
+    Serializer for user authentication using Telegram ID.
+    """
+
     telegram_id = serializers.CharField()
 
     def validate(self, data):
+        """
+        The method authenticates the user using the telegram ID.
+        If there is no user with such a telegram ID, then an exception is raised.
+        """
         telegram_id = data.get("telegram_id")
         user = authenticate(telegram_id=telegram_id)
         if user is None:
@@ -25,6 +37,9 @@ class TelegramAuthSerializer(serializers.Serializer):
 
 
 class CreateTelegramUserSerializer(serializers.ModelSerializer):
+    """
+    The serializer creates a user with the specified username and telegram id.
+    """
 
     username = serializers.CharField(max_length=150, required=True)
     telegram_id = serializers.CharField(max_length=15, required=True)
@@ -34,5 +49,8 @@ class CreateTelegramUserSerializer(serializers.ModelSerializer):
         fields = ["telegram_id", "username"]
 
     def create(self, validated_data: Any):
+        """
+        The method creates and returns a user with the specified data.
+        """
         user = TelegramUser.objects.create(**validated_data)
         return user
