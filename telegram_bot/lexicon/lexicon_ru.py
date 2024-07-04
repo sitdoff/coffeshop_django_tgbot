33d086d6b6adeeed426985102b77e827d9511628
Roman Ivanov <sitdoff@gmail.com>
2024-07-04 13:10:27 +0300

    add lexicon_ru /telegram_bot/lexicon/lexicon_ru.py

---
+++ telegram_bot/lexicon/lexicon_ru.py
@@ -0,0 +1,10 @@
+from typing import Literal
+
+LEXICON_RU: dict[Literal["system"], dict | str] = {
+    "system": {
+        "config_loaded": "Configuration loaded.",
+        "bot_created": "Bot created.",
+        "dispatcher_created": "Dispatcher created.",
+        "bot_config_loaded": "Bot config loaded.",
+    }
+}
