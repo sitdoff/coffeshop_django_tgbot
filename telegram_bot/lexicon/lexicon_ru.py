df90ca402892835a4eaf675601cfa1542c503c92
Roman Ivanov <sitdoff@gmail.com>
2024-07-05 13:56:59 +0300

    add start handler

--- telegram_bot/lexicon/lexicon_ru.py
+++ telegram_bot/lexicon/lexicon_ru.py
@@ -1,6 +1,6 @@
 from typing import Any, Literal
 
-LEXICON_RU: dict[Literal["system"], Any] = {
+LEXICON_RU: dict[Literal["system", "commands"], Any] = {
     "system": {
         "config_loaded": "Configuration loaded.",
         "bot_created": "Bot created.",
@@ -9,5 +9,11 @@ LEXICON_RU: dict[Literal["system"], Any] = {
         "redis_config_loaded": "Redis config loaded.",
         "redis_connection_created": "Redis connection created.",
         "workflow_data_updated": "Workflow data updated.",
-    }
+        "routers_registred": "Routers registred.",
+        "wrong": "Something went wrong.",
+    },
+    "commands": {
+        "start": "Start.",
+        "help": "Help.",
+    },
 }
