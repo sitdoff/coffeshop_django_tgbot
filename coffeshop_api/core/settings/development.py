0c5d82b0922a861fd2bafc589fd05b1d169e8255
Roman Ivanov <sitdoff@gmail.com>
2024-07-05 13:33:37 +0300

    update development settings

--- coffeshop_api/core/settings/development.py
+++ coffeshop_api/core/settings/development.py
@@ -9,6 +9,8 @@ from .base import *
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+ALLOWED_HOSTS = ["web", "localhost", "127.0.0.1"]
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
