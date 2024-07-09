8984e6ea344160fa7c881ab3faaf8b830f2c2521
Roman Ivanov <sitdoff@gmail.com>
2024-07-09 17:15:56 +0300

    rename function get_start_keyboard /telegram_bot/keyboards/callback_keyboards.py

--- telegram_bot/keyboards/callback_keyboards.py
+++ telegram_bot/keyboards/callback_keyboards.py
@@ -2,7 +2,7 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from lexicon.lexicon_ru import LEXICON_RU
 
 
-async def set_start_keyboard() -> InlineKeyboardMarkup:
+async def get_start_keyboard() -> InlineKeyboardMarkup:
     make_order_button = InlineKeyboardButton(text=LEXICON_RU["inline"]["make_order"], callback_data="make_order")
     my_orders_button = InlineKeyboardButton(text=LEXICON_RU["inline"]["my_orders"], callback_data="my_orders")
     keyboard = InlineKeyboardMarkup(
