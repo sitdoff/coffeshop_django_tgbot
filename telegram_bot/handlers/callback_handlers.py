73b6cff8e6250264a406d66c9017de8627db9867
Roman Ivanov <sitdoff@gmail.com>
2024-07-10 08:05:35 +0300

    update process_category_callback /telegram_bot/handlers/callback_handlers.py

--- telegram_bot/handlers/callback_handlers.py
+++ telegram_bot/handlers/callback_handlers.py
@@ -1,20 +1,28 @@
+import logging
+
 import redis
 from aiogram import F, Router
 from aiogram.types import CallbackQuery
-from filters.callback_filters import IsCategoryCallback
+from keyboards.callback_keyboards import CategoryCallbackFactory
 from services.services import get_data_for_answer_category_callback
+
+logger = logging.getLogger(__name__)
 
 router: Router = Router()
 
 
 @router.callback_query(F.data == "make_order")
-@router.callback_query(IsCategoryCallback("category"))
+@router.callback_query(CategoryCallbackFactory.filter())
 async def process_category_callback(
     callback: CallbackQuery,
     redis_connection: redis.Redis,
     api_url: str,
-    category_id: str = None,
+    callback_data: CategoryCallbackFactory | None = None,
 ):
+
+    category_id = callback_data.category_id if callback_data else None
+    logger.debug("Callback data: %s", callback_data)
+
     category_description, keyboard = await get_data_for_answer_category_callback(
         callback, redis_connection, api_url, category_id
     )
