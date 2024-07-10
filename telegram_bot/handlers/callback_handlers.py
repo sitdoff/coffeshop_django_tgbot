25087ebddb8e0928504f9d8ed4b4c35208e2bfa9
Roman Ivanov <sitdoff@gmail.com>
2024-07-10 20:12:22 +0300

    add process_product_callback /telegram_bot/handlers/callback_handlers.py

--- telegram_bot/handlers/callback_handlers.py
+++ telegram_bot/handlers/callback_handlers.py
@@ -3,8 +3,11 @@ from typing import Any, Literal
 
 from aiogram import F, Router
 from aiogram.types import CallbackQuery
-from keyboards.callback_keyboards import CategoryCallbackFactory
-from services.services import get_data_for_answer_category_callback
+from keyboards.callback_keyboards import CategoryCallbackFactory, ProductCallbackFactory
+from services.services import (
+    get_data_for_answer_category_callback,
+    get_data_for_answer_product_callback,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -24,6 +27,28 @@ async def process_category_callback(
     data_for_answer = await get_data_for_answer_category_callback(
         callback, extra["redis_connection"], extra["api_url"], category_id
     )
+
+    await callback.message.edit_media(
+        media=data_for_answer["picture"],
+    )
+    await callback.message.edit_caption(
+        caption=data_for_answer["description"],
+        reply_markup=data_for_answer["keyboard"],
+    )
+
+
+@router.callback_query(ProductCallbackFactory.filter())
+async def process_product_callback(
+    callback: CallbackQuery,
+    extra: dict[Literal["redis_connection", "api_url"], Any],
+    callback_data: ProductCallbackFactory,
+):
+    logger.debug("Product callback data: %s", callback_data)
+
+    data_for_answer = await get_data_for_answer_product_callback(
+        callback, extra["redis_connection"], extra["api_url"], callback_data.product_id
+    )
+    logger.debug("Data for answer: %s", data_for_answer)
 
     await callback.message.edit_media(
         media=data_for_answer["picture"],
