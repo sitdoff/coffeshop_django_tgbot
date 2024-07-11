c8136b46f7ab8ccf66f986371b796870f3154d95
Roman Ivanov <sitdoff@gmail.com>
2024-07-11 13:21:44 +0300

    add CategoryModel, NestedCategoryModel, ProductModel /telegram_bot/models/models.py

---
+++ telegram_bot/models/models.py
@@ -0,0 +1,118 @@
+from decimal import Decimal
+from typing import Any, Optional
+
+from aiogram.types import (
+    FSInputFile,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InputMediaPhoto,
+    URLInputFile,
+)
+from filters.callback_factories import CategoryCallbackFactory, ProductCallbackFactory
+from keyboards.callback_keyboards import set_product_button_text
+from lexicon.lexicon_ru import LEXICON_RU
+from pydantic import BaseModel
+
+
+class ProductModel(BaseModel):
+    id: int
+    name: str
+    picture: InputMediaPhoto | str | None
+    description: str | None
+    category: str | None
+    price: Decimal
+    parent_id: int | None = None
+    keyboard: InlineKeyboardMarkup | None = None
+
+    def __init__(self, /, **data: Any) -> None:
+        super().__init__(**data)
+        self.picture = self.get_picture(data)
+        self.keyboard = self.get_product_inline_keyboard(data)
+
+    def get_product_inline_keyboard(self, data: dict) -> InlineKeyboardMarkup:
+        buttons = [
+            [InlineKeyboardButton(text=LEXICON_RU["inline"]["add_cart"], callback_data="pass")],
+            [
+                InlineKeyboardButton(
+                    text=LEXICON_RU["inline"]["back"],
+                    callback_data=CategoryCallbackFactory(category_id=data["parent_id"]).pack(),
+                )
+            ],
+        ]
+        keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+        return keyboard
+
+    def get_picture(self, data):
+        if data["picture"] is None:
+            return InputMediaPhoto(media=FSInputFile("images/default.jpg"), caption=self.description)
+        return InputMediaPhoto(media=URLInputFile(data["picture"]), caption=self.description)
+
+
+class NestedCategoryModel(BaseModel):
+    id: int
+    name: str
+    url: str
+    description: str | None
+    picture: InputMediaPhoto | str | None
+
+
+class CategoryModel(BaseModel):
+    id: int
+    name: str
+    url: str
+    description: str = LEXICON_RU["system"]["not_found"]
+    picture: InputMediaPhoto | str | None = LEXICON_RU["system"]["not_found"]
+    children: Optional[list[NestedCategoryModel]] = None
+    products: Optional[list[ProductModel]] = None
+    parent: str | None = None
+    parent_id: int | None = None
+    keyboard: InlineKeyboardMarkup | None = None
+
+    def __init__(self, /, **data: Any) -> None:
+        super().__init__(**data)
+        self.picture = self.get_picture(data)
+        self.keyboard = self.get_category_inline_keyboard(data)
+
+    def get_category_inline_keyboard(self, data: dict):
+        buttons = []
+        if data:
+            if data["children"]:
+                buttons = [
+                    [
+                        InlineKeyboardButton(
+                            text=child_category["name"],
+                            callback_data=CategoryCallbackFactory(category_id=child_category["id"]).pack(),
+                        )
+                    ]
+                    for child_category in data["children"]
+                ]
+
+            if data["products"]:
+                buttons = [
+                    [
+                        InlineKeyboardButton(
+                            text=set_product_button_text(product),
+                            callback_data=ProductCallbackFactory(product_id=product["id"]).pack(),
+                        )
+                    ]
+                    for product in data["products"]
+                ]
+                pass
+
+            if data["parent_id"]:
+                buttons.append(
+                    [
+                        InlineKeyboardButton(
+                            text=LEXICON_RU["inline"]["back"],
+                            callback_data=CategoryCallbackFactory(category_id=data["parent_id"]).pack(),
+                        )
+                    ]
+                )
+
+            keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+            return keyboard
+
+    def get_picture(self, data):
+        if data["picture"] is None:
+            return InputMediaPhoto(media=FSInputFile("images/default.jpg"), caption=self.description)
+        return InputMediaPhoto(media=URLInputFile(data["picture"]), caption=self.description)
