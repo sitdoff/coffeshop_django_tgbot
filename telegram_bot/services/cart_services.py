f1c78ec97ae55437ce15a2837ade19802b6416c8
Roman Ivanov <sitdoff@gmail.com>
2024-07-12 13:33:47 +0300

    add get_product_dict_from_redis /telegram_bot/services/cart_services.py

--- telegram_bot/services/cart_services.py
+++ telegram_bot/services/cart_services.py
@@ -5,3 +5,10 @@ def get_product_str_for_redis(
     values = [str(product_dict.get(key, "")) for key in keys]
     return delimeter.join(values)
 
+
+def get_product_dict_from_redis(
+    product_str: str, template: str = "id:name:price:quantity:cost", delimeter: str = ":"
+) -> dict:
+    keys = template.split(delimeter)
+    values = product_str.split(delimeter)
+    return dict(zip(keys, values))
