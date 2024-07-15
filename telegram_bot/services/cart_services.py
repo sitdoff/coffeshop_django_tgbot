fb5365ccc76c172f5b03fed3ed09e28cb81a1fb6
Roman Ivanov <sitdoff@gmail.com>
2024-07-15 09:14:41 +0300

    add CartManaget /telegram_bot/services/cart_services.py

--- telegram_bot/services/cart_services.py
+++ telegram_bot/services/cart_services.py
@@ -1,4 +1,14 @@
-def get_product_dict_from_redis(
+import logging
+
+from aiogram.types import CallbackQuery
+from filters.callback_factories import AddToCartCallbackFactory
+from models.models import ProductModel
+from redis.asyncio import Redis
+
+logger = logging.getLogger(__name__)
+
+
+def get_product_dict_from_redis_cart(
     product_str: str, template: str = "id:name:price:quantity:cost", delimeter: str = ":"
 ) -> dict:
     keys = template.split(delimeter)
@@ -8,3 +18,18 @@ def get_product_dict_from_redis(
 
 def get_product_model_from_redis(product_dict: dict) -> ProductModel:
     return ProductModel(**product_dict, is_data_from_redis=True)
+
+
+class CartManager:
+    def __init__(self, callback: CallbackQuery, callback_data: AddToCartCallbackFactory):
+        self.callback = callback
+        self.callback_data = callback_data
+
+    async def add_product_in_redis_cart(self, redis_connection: Redis) -> None:
+        cart_in_redis = f"cart:{self.callback.from_user.id}"
+        await redis_connection.hset(
+            name=cart_in_redis,
+            key=str(self.callback_data.id),
+            value=self.callback_data.get_product_str_for_redis(),
+        )
+        logger.debug("Product % added to cart", self.callback_data.get_product_str_for_redis())
