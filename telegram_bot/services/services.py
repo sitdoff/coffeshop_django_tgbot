5b52eb4d19b79214ec92fae7a3f755dcb9826ea4
Roman Ivanov <sitdoff@gmail.com>
2024-07-23 19:34:34 +0300

    add comments /telegram_bot/services/services.py

--- telegram_bot/services/services.py
+++ telegram_bot/services/services.py
@@ -3,37 +3,41 @@ import logging
 import aiohttp
 import redis.asyncio as redis
 from aiogram.types import FSInputFile, InputMediaPhoto, Message, URLInputFile
-from keyboards.callback_keyboards import (
-    get_categories_inline_keyboard,
-    get_product_inline_keyboard,
-)
-from lexicon.lexicon_ru import LEXICON_RU
 from models.models import CategoryModel, ProductModel
 
 logger = logging.getLogger(__name__)
 
 
 async def set_auth_token(token: str, message: Message, redis_connection: redis.Redis) -> None:
+    """
+    Записывает токен аутентификации в Redis.
+    """
     await redis_connection.set(f"token:{message.from_user.id}", token)
 
 
 async def get_auth_token(message: Message, redis_connection: redis.Redis) -> str:
+    """
+    Возвращает токен аутентификации из Redis.
+    """
     return await redis_connection.get(f"token:{message.from_user.id}")
 
 
 async def delete_auth_token(message: Message, redis_connection: redis.Redis) -> None:
+    """
+    Удаляет токен аутентификации из Redis.
+    """
     await redis_connection.delete(f"token:{message.from_user.id}")
-
-
-async def get_picture(data: dict) -> InputMediaPhoto:
-    if data["picture"] is None:
-        return InputMediaPhoto(media=FSInputFile("images/default.jpg"))
-    return InputMediaPhoto(media=URLInputFile(data["picture"]))
 
 
 async def authorize_user(
     message: Message, redis_connection: redis.Redis, session: aiohttp.ClientSession, api_url: str
 ) -> str:
+    """
+    Возвращает токен аутентификации.
+
+    Сначала проверяет наличие токена пользователя в Redis. Если его нет в Redis,
+    то отправляет запрос API на получение токена. Полученный от API токен записывает в Redis.
+    """
     token = await get_auth_token(message, redis_connection)
     if not token:
         async with session.post(
@@ -52,6 +56,11 @@ async def get_category_model_for_answer_callback(
     api_url: str,
     category_id: str | int | None = None,
 ) -> CategoryModel:
+    """
+    Возвращает модель категории.
+
+    Запрашивает данные категории у API. Из полученных данных создает модель категории.
+    """
     if category_id:
         url = f"{api_url}/categories/{category_id}/"
     else:
@@ -78,6 +87,11 @@ async def get_product_model_for_answer_callback(
     api_url: str,
     product_id: str | int | None,
 ) -> ProductModel:
+    """
+    Возвращает модель товара.
+
+    Запрашивает данные категории у API. Из полученных данных создает модель товара.
+    """
     url = f"{api_url}/product/{product_id}/"
     logger.debug("Url is %s", url)
 
