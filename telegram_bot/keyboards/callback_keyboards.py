1a137c234694f87d66a5645d115dcf1ff76eb0b4
Roman Ivanov <sitdoff@gmail.com>
2024-07-23 19:38:48 +0300

    add comments /telegram_bot/keyboards/callback_keyboards.py

--- telegram_bot/keyboards/callback_keyboards.py
+++ telegram_bot/keyboards/callback_keyboards.py
@@ -8,10 +8,16 @@ logger = logging.getLogger(__name__)
 
 
 def set_product_button_text(product: dict) -> str:
+    """
+    Возвращает текст для кнопки продукта.
+    """
     return f'{product["name"]} - {product["price"]} руб.'
 
 
 async def get_start_keyboard() -> InlineKeyboardMarkup:
+    """
+    Возвращает инлайн-клавиатуру для сообщения после команды /start.
+    """
     make_order_button = InlineKeyboardButton(text=LEXICON_RU["inline"]["make_order"], callback_data="make_order")
     my_orders_button = InlineKeyboardButton(text=LEXICON_RU["inline"]["my_orders"], callback_data="pass")
     keyboard = InlineKeyboardMarkup(
