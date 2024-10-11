b112eacd74d6aa4b83f840bdedf4e43a6f2548b7
Roman Ivanov <sitdoff@gmail.com>
2024-10-11 13:16:05 +0300

    update /test_cart.py /telegram_bot/tests/test_models/test_cart/

--- telegram_bot/tests/test_models/test_cart/test_cart.py
+++ telegram_bot/tests/test_models/test_cart/test_cart.py
@@ -582,9 +582,30 @@ async def test_cart__add_cart_button(cart: Cart, products: dict[int, ProductMode
     )
 
 
-async def test_cart__edit_product_button():
-    # TODO: написать тест.
-    pass
+async def test_cart__edit_product_button(
+    cart: Cart, products: dict[int, ProductModel], add_callbacks: dict[str, AddToCartCallbackFactory]
+):
+    product_1 = products.get(1)
+    product_1_keyboard_buttons = product_1.keyboard.inline_keyboard
+
+    assert len(product_1_keyboard_buttons[0]) == 1
+    assert product_1_keyboard_buttons[0][0].text == LEXICON_RU["inline"]["add_cart"]
+    assert product_1_keyboard_buttons[0][0].callback_data == AddToCartCallbackFactory(**product_1.model_dump()).pack()
+
+    product_1_keyboard_buttons = await cart._edit_product_button(product_1_keyboard_buttons)
+
+    assert len(product_1_keyboard_buttons[0]) == 1
+    assert product_1_keyboard_buttons[0][0].text == LEXICON_RU["inline"]["product_quantity_in_cart"].substitute(count=0)
+    assert product_1_keyboard_buttons[0][0].callback_data == AddToCartCallbackFactory(**product_1.model_dump()).pack()
+
+    await cart.add_product_in_cart(add_callbacks["1"])
+    await cart.get_items_from_redis()
+
+    product_1_keyboard_buttons = await cart._edit_product_button(product_1_keyboard_buttons)
+
+    assert len(product_1_keyboard_buttons[0]) == 1
+    assert product_1_keyboard_buttons[0][0].text == LEXICON_RU["inline"]["product_quantity_in_cart"].substitute(count=1)
+    assert product_1_keyboard_buttons[0][0].callback_data == AddToCartCallbackFactory(**product_1.model_dump()).pack()
 
 
 async def test_cart_get_edit_cart_inline_keyboard():
