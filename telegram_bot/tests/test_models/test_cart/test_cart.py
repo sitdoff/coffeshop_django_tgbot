6b2052945e46b5fb3b6bbb240ca3e6562ea0d36d
Roman Ivanov <sitdoff@gmail.com>
2024-10-11 13:48:28 +0300

    update test_cart.py /telegram_bot/tests/test_models/test_cart/

--- telegram_bot/tests/test_models/test_cart/test_cart.py
+++ telegram_bot/tests/test_models/test_cart/test_cart.py
@@ -608,9 +608,45 @@ async def test_cart__edit_product_button(
     assert product_1_keyboard_buttons[0][0].callback_data == AddToCartCallbackFactory(**product_1.model_dump()).pack()
 
 
-async def test_cart_get_edit_cart_inline_keyboard():
-    # TODO: написать тест.
-    pass
+async def test_cart_get_edit_cart_inline_keyboard(cart: Cart, add_callbacks: dict[str, AddToCartCallbackFactory]):
+
+    def check_inline_keyboard(inline_keyboard: InlineKeyboardMarkup):
+        assert isinstance(inline_keyboard, InlineKeyboardMarkup)
+
+        buttons = inline_keyboard.inline_keyboard
+
+        for row in buttons:
+            for button in row:
+                assert isinstance(button, InlineKeyboardButton)
+
+        assert len(buttons) == len(cart.items) + 1
+
+    inline_keyboard = await cart.get_edit_cart_inline_keyboard()
+    check_inline_keyboard(inline_keyboard)
+
+    await cart.add_product_in_cart(add_callbacks["1"])
+    await cart.get_items_from_redis()
+
+    inline_keyboard = await cart.get_edit_cart_inline_keyboard()
+    check_inline_keyboard(inline_keyboard)
+
+    assert inline_keyboard.inline_keyboard[0][0].text == add_callbacks["1"].name
+    assert (
+        inline_keyboard.inline_keyboard[0][0].callback_data
+        == ProductCallbackFactory(product_id=add_callbacks["1"].id).pack()
+    )
+
+    await cart.add_product_in_cart(add_callbacks["2"])
+    await cart.get_items_from_redis()
+
+    inline_keyboard = await cart.get_edit_cart_inline_keyboard()
+    check_inline_keyboard(inline_keyboard)
+
+    assert inline_keyboard.inline_keyboard[1][0].text == add_callbacks["2"].name, "Button 2 text is wrong"
+    assert (
+        inline_keyboard.inline_keyboard[1][0].callback_data
+        == ProductCallbackFactory(product_id=add_callbacks["2"].id).pack()
+    )
 
 
 async def test_cart_edit_category_inline_keyboard():
