4db8082f54c8f2f50ba589296081f24a0ce92e90
Roman Ivanov <sitdoff@gmail.com>
2024-10-24 09:12:45 +0300

    add RedisSingleton /telegram_bot/services/redis_services.py

---
+++ telegram_bot/services/redis_services.py
@@ -0,0 +1,34 @@
+from config_data.config import RedisConfig
+from redis.asyncio import ConnectionPool, Redis
+
+
+class RedisSengleton:
+    """
+    Класс предоставляет пул соединений с Redis.
+    """
+
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(RedisSengleton, cls).__new__(cls)
+        return cls._instance
+
+    async def init_pool(self, redis_config: RedisConfig):
+        """
+        Метод создаёт и возвращает пул соединений.
+        """
+        if not hasattr(self, "_redis_pool"):
+            # TODO: Попробовать сделать так, чтобы можно было просто распаковать словарь из модели конфига
+            # примерно так ConnectionPool(**redis_config.dict())
+            self._redis_pool = ConnectionPool(host=redis_config.redis_host, port=redis_config.redis_port)
+        return self._redis_pool
+
+    async def get_pool(self):
+        """
+        Метод возвращает уже существующий пул соединений.
+        """
+        return self._redis_pool
+
+
+redis_connection_pool = RedisSengleton()
